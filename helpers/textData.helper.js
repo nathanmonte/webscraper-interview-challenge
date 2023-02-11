30eb4b96d5aaba6cc7a03c865871a8743a57e7cd
Nathan Monte <nathan.monte@givingforce.com>
2023-02-11 15:45:47 +0000

    Implement basic link functionality.

--- helpers/textData.helper.js
+++ helpers/textData.helper.js
@@ -1,3 +1,21 @@
 export const getTitle = (document) => document.querySelector("title").text;
 
 export const getDescription = (document) => document.querySelector("meta[name='description']").content;
+
+export const getLinks = (document, domain) => {
+    const links = Array.prototype.slice.call(document.querySelectorAll("a")).map(image => image.href);
+    const linksWithoutEmpties = links.filter(link => link.length > 0);
+    const linksModified = linksWithoutEmpties.map(link => {
+        // Add the domain if missing
+        if (link[0] === "/") link = `${domain}${link}`;
+        // TODO: Review if I want this link type.
+        if (link.includes("?")) link = link.split("?")[0];
+        return link;
+    })
+    
+    const linksWithoutNonDomainValues = linksModified.filter(link => link.includes("http://") || link.includes("https://"));
+
+    const uniqueDomains = Array.from(new Set(linksWithoutNonDomainValues)).sort();
+
+    return uniqueDomains;
+}
--- scraper.js
+++ scraper.js
@@ -1,21 +1,26 @@
 
 import { getImageSources } from "./helpers/images.helper.js";
-import { getDescription, getTitle } from "./helpers/textData.helper.js";
+import { getDescription, getLinks, getTitle } from "./helpers/textData.helper.js";
 import { getDocumentForUrl } from "./helpers/webAccess.helper.js";
 
 (async () => {
 
     const url = 'https://www.github.com';
-    const domain = "github.com";
+    const domain = "https://github.com";
+    const domainLocation = "github.com"
 
     const document = await getDocumentForUrl(url);
 
+    const description = getDescription(document);
+
     const pageData = {
-        imageSources: getImageSources(document, domain),
+        imageSources: getImageSources(document, domainLocation),
         title: getTitle(document),
-        description: getDescription(document)
+        description: description ? description : "🚨 Description not available for page.",
+        links: getLinks(document, domain)
     }
 
-    console.log(pageData)
+    
+    console.log(pageData);
 })();
     
